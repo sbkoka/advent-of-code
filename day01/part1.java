c236009c2c91ed5c33bc610640e2a933a94890a2
Sanjana Koka <sanjana@Sanjana-MacBook-Pro.local>
2024-12-02 09:19:27 -0800

    part1

---
+++ day01/part1.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class part1{
+
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            
+        }
+    }
+}
---
+++ day02/part1.py
@@ -0,0 +1,29 @@
+
+num_safe = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        levels = line.split(" ")
+        increasing = True
+        safe = True
+        
+        for i in range(1, len(levels)):
+            level = int(levels[i])
+            prev = int(levels[i - 1])
+            if i == 1:
+                if prev < level:
+                    increasing = True
+                elif prev > level:
+                    increasing = False
+                else: 
+                    safe = False
+                    break
+            if (increasing and prev >= level) or (not increasing and prev <= level) or abs(prev - level) > 3:
+                safe = False
+                #print("not safe", line)
+                break
+        if safe:
+            num_safe += 1
+            #print("safe", line)
+print(num_safe)
+
+            
